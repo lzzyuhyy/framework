799b23daaad7f00b673ea8d578bfd6d4731c79c6
zzsuza <17683844709@163.com>
2024-07-31 21:07:21 +0800

    viper read nacos config

--- nacos/handle.go
+++ nacos/handle.go
@@ -3,10 +3,15 @@ package nacos
 import (
 	"github.com/nacos-group/nacos-sdk-go/v2/model"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"github.com/spf13/viper"
 )
 
 // 发现服务
-func RegisterServiceInstance(address string, port uint64, serverName, groupName string) error {
+func RegisterServiceInstance() error {
+	address := viper.GetString("nacos.address")
+	port := viper.GetUint64("nacos.port")
+	serverName := viper.GetString("nacos.servername")
+	groupName := viper.GetString("nacos.group")
 	cli, err := newNacosClient(address, port)
 	if err != nil {
 		panic(err)
